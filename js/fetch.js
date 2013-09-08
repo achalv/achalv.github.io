57838ff1591e98b794cba2eaa7aa4e97b8106978
Achal Varma <achalv2.0@gmail.com>
2013-09-08 15:50:48 -0500

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -41,7 +41,7 @@ function githubHandler(data){
 			li.innerHTML+= "<div class='date'>"+date+"</div>";
 			li.setAttribute('class', 'original');
 			li.innerHTML+= "<p>" + repo.description + "</p>";
-			li.innerHTML+= "<p>" + data + "</p>";
+			li.innerHTML+= "<p>" + repo.language + "</p>";
 		}
 		ul.appendChild(li);
 		return true;
