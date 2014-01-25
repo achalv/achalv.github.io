1f1bc343531309bb553173f9932b9d6ae13fea3c
Achal Varma <achalv2.0@gmail.com>
2014-01-25 14:04:28 -0600

    Pausing work on this for now.

--- js/fetch.js
+++ js/fetch.js
@@ -33,7 +33,9 @@ function githubHandler(data){
 		var li = document.createElement("li");
 		li.innerHTML+= "<h2 class='repo-name'><a target='_blank' href='"
 						+repo.html_url+"'>" + repo.name + "</a></h2>";
-										
+		
+		li.innerHTML+= "<div class='gh-controls'><i class='fa fa-star star'></i><i class='fa fa-code-fork fork'></i></div>";
+
 		li.innerHTML+= "<div class='metadata'>";
 		
 		var project_language = repo.langauge;
@@ -41,10 +43,8 @@ function githubHandler(data){
 		var date_lastUpdated = ((date.getMonth()+1) + '/' + date.getDate() + '/' +  date.getFullYear());
 		
 		//li.innerHTML+= "<p class='tag'>" + project_language + "</p>";
-		li.innerHTML+= "<p class='last-updated'>Updated: "  + project_language + "</p>";
 		li.innerHTML+= "<p class='last-updated'>Updated: "  + date_lastUpdated + "</p>";
 		li.innerHTML+= "</div>";
-		li.innerHTML+= "<div class='gh-controls'><i class='fa fa-star star'></i><i class='fa fa-code-fork fork'></i></div>";
 
 		if (repo.fork == true){
 			li.setAttribute('class', 'fork');
