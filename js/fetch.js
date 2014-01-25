6aaaaf65d7022eb26ae2e363a69247db87e646f6
Achal Varma <achalv2.0@gmail.com>
2014-01-25 13:45:14 -0600

    So, so stupid FML

--- js/fetch.js
+++ js/fetch.js
@@ -34,7 +34,7 @@ function githubHandler(data){
 		li.innerHTML+= "<h2 class='repo-name'><a target='_blank' href='"
 						+repo.html_url+"'>" + repo.name + "'</a></h2>";
 						
-		li.innerHTML=+ "<div class='gh-controls'><i class='fa fa-star star'></i><i class='fa fa-code-fork fork'></i></div>";				
+		li.innerHTML+= "<div class='gh-controls'><i class='fa fa-star star'></i><i class='fa fa-code-fork fork'></i></div>";				
 		li.innerHTML+= "<div class='metadata'>";
 		
 		var project_language = repo.langauge;
